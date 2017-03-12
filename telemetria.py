49ece0b6913c28456233b9c4b23a544bab56eb30
pepeg1992 <pepegiovanni_92@hotmail.it>
2017-03-12 09:57:26 +0100

    Update telemetria.py

--- telemetria.py
+++ telemetria.py
@@ -3,7 +3,7 @@ import serial
 import struct
 arduino = serial.Serial('/dev/ttyACM0',115200)
 
-print 'rpm\tmap\tair\tlam\ttps\tengt\tbat\toilp\toilt\tgear\tfuel\tvel\n'
+print 'rpm\tmap\tair\tlam\ttps\tengt\tbat\toilp\toilt\tgear\tfuel\tvel\bse\tps2\tpd1\tpd2\n'
 while True:
     f=open('{:%Y%m%d%H%M}.csv'.format(datetime.datetime.now()), 'a')
     for i in range (0,600): #600 campioni per file
