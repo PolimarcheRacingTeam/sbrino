1a5d19f60f3ebbd64c3684601b766359cb54931c
sebgiles <mrsebgiles@gmail.com>
2017-03-24 00:40:43 +0100

    telemetria completa

--- telemetria.py
+++ telemetria.py
@@ -1,9 +1,9 @@
 import datetime
 import serial
 import struct
-arduino = serial.Serial('/dev/ttyACM0',115200)
+arduino = serial.Serial('/dev/cu.usbmodem14121',115200)
 
-print 'rpm\tmap\tair\tlam\ttps\tengt\tbat\toilp\toilt\tgear\tfuel\tvel\bse\tps2\tpd1\tpd2\n'
+print 'rpm\tmap\tair\tlam\ttps\tengt\tbat\toilp\toilt\tgear\tfuel\tvel\tbse\tps2\tpd1\tpd2\n'
 while True:
     f=open('{:%Y%m%d%H%M}.csv'.format(datetime.datetime.now()), 'a')
     for i in range (0,600): #600 campioni per file
@@ -11,13 +11,13 @@ while True:
             pass
         if arduino.read() != '\xff':
             continue
-        data = arduino.read(24)
-        val = struct.unpack('<12H',data)
+        data = arduino.read(64) #byte grezzi
+        val = struct.unpack('<16HL8H6h',data) #valori
 
         for j in range(0, 16):
             print '{0}\t'.format(val[j]),
         print '\r',
 
-        for j in range(0, 16):
-            f.write("%d;" % val[j])
-        f.write('/n')
+        for el in val:
+            f.write("%d;" % el)
+        f.write('\n')
